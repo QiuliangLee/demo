0432efde51a8fe72fc1f8b0dec7c3b47afc9b5a8
liqiuliang <liqiuliang@bilibili.com>
2023-10-20 03:05:26 +0800

    2-2

--- src/main/java/com/example/demo/Person.java
+++ src/main/java/com/example/demo/Person.java
@@ -6,7 +6,7 @@ package com.example.demo;
  */
 public class Person {
     public static void main(String[] args) {
-        System.out.println("1");
+        System.out.println("3");
     }
 
     public void test(){
